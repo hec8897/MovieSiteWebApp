cd0a27c40495090486fe01516ade9b9b849d998a
hec8897 <37240562+hec8897@users.noreply.github.com>
2019-02-18 15:34:44 +0900

    no message

--- MovieSiteWebApp/html/js/main.js
+++ MovieSiteWebApp/html/js/main.js
@@ -1,7 +1,44 @@
-$(document).ready(function(){
-    $(".ham_btn").click(function(){
+$(document).ready(function () {
+    var clickNum = 0;
+    $(".ham_btn").click(function () {
+        console.log(clickNum);
+        clickNum++;
+
+        if(clickNum == 1){
+            
         $("nav").animate({
-            "left":"0%"
+            "left": "0%"
         })
+
+        $(this).children("div:nth-child(2)").hide();
+     
+        $(this).children("div:nth-child(1)").css({
+            "transform": "rotate(-45deg)",
+            "top": "7px"
+        })
+        $(this).children("div:nth-child(3)").css({
+            "transform": "rotate(45deg)"
+        })
+
+        }
+        else{
+
+            $(this).children("div:nth-child(2)").show();
+     
+            $(this).children("div:nth-child(1)").css({
+                "transform": "rotate(0deg)",
+                "top": "0px"
+            })
+            $(this).children("div:nth-child(3)").css({
+                "transform": "rotate(0deg)"
+            })
+            $("nav").animate({
+                "left": "-100%"
+            })
+            return clickNum = 0;
+
+        }
+
     })
-})
+
+})   
